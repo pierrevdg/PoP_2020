1ddb2f73b10910bb0a5239f463d6ea6c4ad6c200
Pierre VANDERGOOTEN <pierre.vandergooten@edu.devinci.fr>
2020-11-29 12:43:44 +0200

    modif

---
+++ Lesson_04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Task_01()
+        {
+            int number;
+            double sum = 0;
+            double average;
+            for(int i = 1; i<=10;i++)
+            {
+                Console.WriteLine("Write a number");
+                number = Convert.ToInt32(Console.ReadLine());
+                sum = sum + number;
+                Console.Clear();
+            }
+            average = sum / 10;
+            Console.WriteLine("Sum : " + sum);
+            Console.WriteLine("Average : " + average);
+        }
+        static void Task_02()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Task_01();
+        }
+    }
+}

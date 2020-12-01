f5dd3a09f2a898feb2669a02db716ab7bdb8be70
Pierre VANDERGOOTEN <pierre.vandergooten@edu.devinci.fr>
2020-12-01 15:38:55 +0200

    Modif

--- Lesson_04/Program.cs
+++ Lesson_04/Program.cs
@@ -9,7 +9,7 @@ namespace Lesson_04
             int number;
             double sum = 0;
             double average;
-            for(int i = 1; i<=10;i++)
+            for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine("Write a number");
                 number = Convert.ToInt32(Console.ReadLine());
@@ -22,11 +22,41 @@ namespace Lesson_04
         }
         static void Task_02()
         {
-
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Task_03()
+        {
+            int num = 1;
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(num);
+                    num++;
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Task_05(int num)
+        {
+            int result = 1;
+            while (num >= 1)
+            {
+                result = result * num;
+                num--;
+            }
+            Console.WriteLine(result);
         }
         static void Main(string[] args)
         {
-            Task_01();
+            Task_05(4);
         }
     }
 }
